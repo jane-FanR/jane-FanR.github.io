6919a2e63a1869a237f642b579652f64bebfa87a
Rafael Carvalho <87777432+RafaelMeslho@users.noreply.github.com>
2023-12-07 13:52:27 +0000

    Create gallery.js

---
+++ scripts/gallery.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //references
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    //references all elements
+    var thumbnails = document.querySelectorAll(".thumbnail");
+
+    thumbnails.forEach(function (thumbnail) {
+        thumbnail.onclick = function () {
+            //display the modal
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        };
+    });
+
+    //close button
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+    };
+    //second option to close
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+});
